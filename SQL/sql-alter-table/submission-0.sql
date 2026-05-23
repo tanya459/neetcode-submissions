77fb6811f6bd7a8b7b3b3905e9c940ca167e31b8
Tannya Sinha <tanyasinha1514@gmail.com>
2026-05-23 19:47:16 +0530

    Add: sql-alter-table - submission-0

---
+++ SQL/sql-alter-table/submission-0.sql
@@ -0,0 +1,22 @@
+CREATE TABLE books (
+  id INTEGER,
+  title TEXT,
+  author TEXT
+);
+-- Do not modify above this line --
+
+ALTER TABLE books add column published_year integer;
+
+ALTER TABLE books Rename column id to isbn;
+
+ALTER TABLE books drop column author;
+
+
+
+
+
+-- Do not modify below this line --
+SELECT column_name, data_type, column_default
+FROM information_schema.columns
+WHERE table_name = 'books'
+ORDER BY column_name;
